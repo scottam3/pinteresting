7e3ed68259db568fd589a605801210f22f354990
scott murtaugh <scott.murtaugh@gmail.com>
2016-07-17 11:57:47 -0400

    fixed destory and edit error as a current users

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -25,17 +25,12 @@ class PinsController < ApplicationController
 
   # POST /pins
   # POST /pins.json
-  def create
-    @pin = current_user.pins.build (pin_params)
-
-    respond_to do |format|
-      if @pin.save
-        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
-        format.json { render :show, status: :created, location: @pin }
-      else
-        format.html { render :new }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
-      end
+def create
+    @pin = current_user.pins.build(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
@@ -57,10 +52,7 @@ class PinsController < ApplicationController
   # DELETE /pins/1.json
   def destroy
     @pin.destroy
-    respond_to do |format|
-      format.html { redirect_to pins_url, notice: 'Pin was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to pins_url
   end
 
   private
@@ -70,8 +62,8 @@ class PinsController < ApplicationController
     end
 
     def correct_user
-      @pin = current_user.pins.find_by(params[:id])
-      redirect_to pins.path if @pin.nil?
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
